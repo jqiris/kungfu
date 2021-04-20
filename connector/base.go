48a344517ed9d329f7af8671c307180fc0cd246a
jmesyan <jieqiang1017@gmail.com>
2021-04-20 23:15:29 +0800

    add zinx

--- connector/base.go
+++ connector/base.go
@@ -1,47 +1,24 @@
 package connector
 
 import (
-	"fmt"
 	"github.com/jqiris/kungfu/coder"
 	"github.com/jqiris/kungfu/rpcx"
 	"github.com/jqiris/kungfu/treaty"
-	"net"
-	"net/http"
+	"github.com/jqiris/zinx/ziface"
+	"github.com/jqiris/zinx/znet"
 )
 
 type BaseConnector struct {
 	Server       *treaty.Server
 	Rpcx         rpcx.RpcBalancer
-	ClientServer *http.Server
+	ClientServer ziface.IServer
 	ClientCoder  coder.Coder
 }
 
 func (b *BaseConnector) Init() {
 	//run the front server
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", b.Server.ClientPort))
-	if err != nil {
-		logger.Fatal(err)
-		return
-	}
-	go b.RunClientServer(listener)
-}
-
-func (b *BaseConnector) RunClientServer(listener net.Listener) {
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			logger.Error(err)
-			return
-		}
-		go b.DealClientConnection(conn)
-	}
-}
-func (b *BaseConnector) DealClientConnection(conn net.Conn) {
-	defer conn.Close()
-	for {
-		buf := make([]byte, 1024)
-
-	}
+	b.ClientServer = znet.NewServer()
+	b.ClientServer.Serve()
 }
 
 func (b *BaseConnector) AfterInit() {
