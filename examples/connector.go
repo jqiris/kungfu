078be501fcbd0c68b9141fb0a0a7b70501ef30af
jqiris <jieqiang1017@163.com>
2021-05-24 00:04:55 +0800

    login

--- examples/connector.go
+++ examples/connector.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/jqiris/kungfu/conf"
 	"github.com/jqiris/kungfu/connector"
+	"github.com/jqiris/kungfu/discover"
 	"github.com/jqiris/kungfu/helper"
 	"github.com/jqiris/kungfu/launch"
 	"github.com/jqiris/kungfu/session"
@@ -13,6 +14,7 @@ import (
 
 type MyConnector struct {
 	connector.BaseConnector
+
 }
 
 func (b *MyConnector) EventHandleSelf(req []byte) []byte {
@@ -24,7 +26,7 @@ func (b *MyConnector) EventHandleBroadcast(req []byte) []byte {
 	fmt.Printf("MyConnector EventHandleBroadcast received: %+v \n", string(req))
 	return nil
 }
-
+//Login 登录操作
 func (b *MyConnector) Login(request tcpserver.IRequest) {
 
 	//先读取客户端的数据
@@ -35,21 +37,23 @@ func (b *MyConnector) Login(request tcpserver.IRequest) {
 	//回复对象
 	conn := request.GetConnection()
 	//解析登录数据
-	loginRequest := &treaty.LoginRequest{}
-	if err := GetRequest(request, loginRequest); err != nil {
+	req := &treaty.LoginRequest{}
+	if err := GetRequest(request, req); err != nil {
 		resp.Code = 1
 		resp.Msg = err.Error()
 		SendMsg(conn, treaty.MsgId_Msg_Login_Response, resp)
+		return
 	}
-	logger.Printf("login request is:%+v", loginRequest)
+	logger.Printf("login request is:%+v", req)
 	//判断登录信息的正确性
-	uid, nickname := loginRequest.Uid, loginRequest.Nickname
+	uid, nickname := req.Uid, req.Nickname
 	tokenkey := conf.GetConnectorConf().TokenKey
 	token := helper.Md5(fmt.Sprintf("%d|%s|%s", uid, nickname, tokenkey))
-	if loginRequest.Token != token {
+	if req.Token != token {
 		resp.Code = 1
 		resp.Msg = "token不正确"
 		SendMsg(conn, treaty.MsgId_Msg_Login_Response, resp)
+		return
 	}
 	//检查会话信息
 	sess := session.GetSession(uid)
@@ -57,6 +61,30 @@ func (b *MyConnector) Login(request tcpserver.IRequest) {
 		if sess.Connector != nil && sess.Connector.ServerId != request.GetServerID() {
 			//之前在其他客户端登录，通知其他connetor登出
 		}
+		//如果连接了后端服务器，进行重连
+		if sess.Backend != nil && sess.Backend != req.Backend{
+			resp.Code = 2
+			resp.Msg = "请进行重连服务器"
+			resp.Backend = sess.Backend
+			return
+		}
+	}
+
+	if req.Backend != nil{
+		//与后端服务器建立连接
+		backend := discover.GetServerById(req.Backend.ServerId)
+		if backend == nil{
+			//查找同类服务器
+			backend  = discover.GetServerByType(req.Backend.ServerType,conn.RemoteAddr().String())
+		}
+		if backend == nil{
+			resp.Code = 3
+			resp.Msg = "找不到服务器"
+			return
+		}
+	} else {
+		//加入大厅用户
+
 	}
 	resp.Code = 0
 	resp.Msg = "登录成功"
