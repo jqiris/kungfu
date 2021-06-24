0f96e808739e14f1fc52f497796076ffcc37eb8b
jmesyan <jieqiang1017@gmail.com>
2021-06-24 19:14:36 +0800

    nats encoder

--- tests/nats_test.go
+++ tests/nats_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/nats-io/nats.go"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestNatsEncoder(t *testing.T) {
@@ -16,14 +17,31 @@ func TestNatsEncoder(t *testing.T) {
 	nc, _ := nats.Connect(url)
 	c, _ := nats.NewEncodedConn(nc, rpcx.NATS_ENCODER)
 	defer c.Close()
-	c.Subscribe("/nats_test", func(resp *treaty.LoginRequest) {
-		fmt.Println("sub resp:", resp)
+	c.Subscribe("/nats_test", func(subj, reply string, req *treaty.LoginRequest) {
+		fmt.Printf("sub subj:%v,reply:%v, req:%+v\n", subj, reply, req)
+		c.Publish(reply, &treaty.LoginResponse{
+			Code:    0,
+			Msg:     "success",
+			Backend: nil,
+		})
 	})
-	c.Publish("/nats_test", &treaty.LoginRequest{
+	//c.Publish("/nats_test", &treaty.LoginRequest{
+	//	Uid:      111,
+	//	Nickname: "jason",
+	//	Token:    "dfs",
+	//	Backend:  nil,
+	//})
+	resp := &treaty.LoginResponse{}
+	if err := c.Request("/nats_test", &treaty.LoginRequest{
 		Uid:      111,
 		Nickname: "jason",
 		Token:    "dfs",
 		Backend:  nil,
-	})
+	}, resp, 10*time.Second); err == nil {
+		fmt.Printf("sub resp:%v", resp)
+	} else {
+		fmt.Errorf("sub resp error: %v", err)
+	}
+
 	select {}
 }
