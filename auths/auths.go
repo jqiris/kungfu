7aecefe7a25337665cfcc67d7cd13449e5815c0a
jmesyan <jieqiang1017@gmail.com>
2022-05-18 10:02:49 +0800

    通讯加密

--- auths/auths.go
+++ auths/auths.go
@@ -2,9 +2,15 @@ package auths
 
 import (
 	"encoding/base64"
+	"errors"
 
 	"github.com/farmerx/gorsa"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/wumansgy/goEncrypt"
+)
+
+var (
+	json = jsoniter.ConfigCompatibleWithStandardLibrary
 )
 
 type Encipherer struct {
@@ -75,4 +81,65 @@ func (e *Encipherer) AesCbcEncrypt(data []byte) (string, error) {
 
 func (e *Encipherer) GetAesSecretKey() (string, string) {
 	return string(e.aesKey), string(e.aesIv)
+}
+
+type DecryptType int
+
+const (
+	_                    DecryptType = iota
+	DecryptTypeAes                   //1-aes解密
+	DecryptTypeRsaPrikey             //2-rsa私钥解密
+	DecryptTypeRsaPubkey             //2-rsa公钥解密
+)
+
+type EncryptType int
+
+const (
+	_                    EncryptType = iota
+	EncryptTypeAes                   //1-aes加密
+	EncryptTypeRsaPrikey             //2-rsa私钥加密
+	EncryptTypeRsaPubkey             //2-rsag公钥加密
+)
+
+func DecryptData(ep *Encipherer, typ DecryptType, src string, res any) error {
+	var data []byte
+	var err error
+	switch typ {
+	case DecryptTypeAes:
+		data, err = ep.AesCbcDecrypt(src)
+		if err != nil {
+			return err
+		}
+	case DecryptTypeRsaPrikey:
+		data, err = ep.RsaPrikeyDecrypt(src)
+		if err != nil {
+			return err
+		}
+	case DecryptTypeRsaPubkey:
+		data, err = ep.RsaPubkeyDecrypt(src)
+		if err != nil {
+			return err
+		}
+	}
+	err = json.Unmarshal(data, res)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func EncryptData(ep *Encipherer, typ EncryptType, src any) (string, error) {
+	data, err := json.Marshal(src)
+	if err != nil {
+		return "", err
+	}
+	switch typ {
+	case EncryptTypeAes:
+		return ep.AesCbcEncrypt(data)
+	case EncryptTypeRsaPrikey:
+		return ep.RsaPrikeyEncrypt(data)
+	case EncryptTypeRsaPubkey:
+		return ep.RsaPubkeyEncrypt(data)
+	}
+	return "", errors.New("no suit type")
 }
