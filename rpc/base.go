1fc8493ab9070ffb41cdf2cac4c94f3d678f5da6
jmesyan <jieqiang1017@gmail.com>
2022-08-24 19:39:52 +0800

    维护消息放入框架

--- rpc/base.go
+++ rpc/base.go
@@ -154,3 +154,32 @@ func (s *ServerBase) HandleBroadcastEvent(req *MsgRpc) []byte {
 	}
 	return resp
 }
+
+func (s *ServerBase) ServerMaintain(req *treaty.ServerMaintainReq) {
+	logger.Warnf("ServerMaintain notice:%+v", req)
+	serverId, reqState := req.ServerId, req.ReqState
+	server := s.Server
+	if serverId != server.ServerId {
+		logger.Errorf("maintain server not current server,current:%v,req:%v", server.ServerId, serverId)
+		return
+	}
+	if reqState == 1 && !server.Maintained { //进入维护
+		//删除服务负载量
+		server.Maintained = true
+		server.Silent = 0
+		err := discover.Register(server)
+		if err != nil {
+			logger.Error(err)
+		}
+		logger.Reportf("服务器:%v,进入维护状态", serverId)
+	} else if reqState == 2 && server.Maintained { //解除维护
+		//设置服务负载量
+		server.Maintained = false
+		server.Silent = 0
+		err := discover.Register(server)
+		if err != nil {
+			logger.Error(err)
+		}
+		logger.Reportf("服务器:%v,解除维护状态", serverId)
+	}
+}
