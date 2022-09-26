9360640cdfb7857f402eba2992a8d94740bcec05
jmesyan <jieqiang1017@gmail.com>
2022-09-26 16:08:11 +0800

    id自增器

--- auths/identity.go
+++ auths/identity.go
@@ -2,31 +2,60 @@ package auths
 
 import (
 	"sync"
+	"time"
 
 	"github.com/jqiris/kungfu/v2/logger"
 	"github.com/jqiris/kungfu/v2/stores"
+	"github.com/jqiris/kungfu/v2/utils"
 )
 
 type IdSourcer func(key string) (int64, error)
 type IdStorer func(key string, id int64) error
 
 type IdGenerator struct {
-	table    string
-	key      string
-	idSource IdSourcer
-	idStore  IdStorer
-	lock     sync.Mutex
-	nextId   int64
+	table     string
+	key       string
+	idSource  IdSourcer
+	idStore   IdStorer
+	lock      sync.Mutex
+	nextId    int64
+	storeTime time.Duration
+	storeId   int64
 }
 
-func NewIdGenerator(table string, key string, idSource IdSourcer, idStore IdStorer) *IdGenerator {
-	return &IdGenerator{
-		table:    table,
-		key:      key,
-		idSource: idSource,
-		idStore:  idStore,
-		lock:     sync.Mutex{},
-		nextId:   -1,
+type IdOption func(ig *IdGenerator)
+
+func WithIdStoreTime(storeTime time.Duration) IdOption {
+	return func(ig *IdGenerator) {
+		ig.storeTime = storeTime
+	}
+}
+
+func NewIdGenerator(table string, key string, idSource IdSourcer, idStore IdStorer, options ...IdOption) *IdGenerator {
+	ig := &IdGenerator{
+		table:     table,
+		key:       key,
+		idSource:  idSource,
+		idStore:   idStore,
+		lock:      sync.Mutex{},
+		nextId:    -1,
+		storeTime: 1 * time.Minute,
+		storeId:   -1,
+	}
+	for _, option := range options {
+		option(ig)
+	}
+	go utils.SafeRun(func() {
+		ig.schedule()
+	})
+	return ig
+}
+func (ig *IdGenerator) schedule() {
+	ticker := time.NewTicker(ig.storeTime)
+	for range ticker.C {
+		if err := ig.Store(); err != nil && !stores.IsRedisNull(err) {
+			logger.Error(err)
+		}
 	}
 }
 
@@ -66,5 +95,13 @@ func (g *IdGenerator) Store() error {
 	if err := stores.HGet(g.table, g.key, &id); err != nil {
 		return err
 	}
-	return g.idStore(g.key, id)
+	if g.storeId == id {
+		return nil
+	}
+	logger.Infof("id generator store:%d", id)
+	err := g.idStore(g.key, id)
+	if err == nil {
+		g.storeId = id
+	}
+	return err
 }
